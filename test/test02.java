d42e02db23de4b136dad72772d78ba8ffdccf706
Swetha V <swethasep1598@gmail.com>
2022-09-14 19:08:55 +0530

    ASUS

--- test/test02.java
+++ test/test02.java
@@ -4,14 +4,14 @@ public class TG01 {
 	// public static void main(String[] args) {
 	// TODO Auto-generated method stub
 	public void D() {
-		System.out.println("Danyy");
+		System.out.println("Danyy A");
 	}
 
 	public void E() {
-		System.out.println("Evangeline");
+		System.out.println("Evangeline F");
 	}
 
 	public void F() {
-		System.out.println("Frank");
+		System.out.println("Frank G");
 	}
 }
