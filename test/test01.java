ea499f2594ab3b3d03bca6e2fe49f52551d7de02
Swetha <swethasep1598@gmail.com>
2022-09-14 18:57:53 +0530

    1st Commit

---
+++ test/test01.java
@@ -0,0 +1,19 @@
+package test;
+
+public class test01 {
+
+	public void A()
+	{
+		System.out.println("Anu");
+	}
+	public void B()
+	{
+		System.out.println("Bindu");
+	}
+	public void C()
+	{
+		System.out.println("Catherine");
+	}
+
+
+}
---
+++ test/test02.java
@@ -0,0 +1,17 @@
+
+public class TG01 {
+
+	// public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	public void D() {
+		System.out.println("Danyy");
+	}
+
+	public void E() {
+		System.out.println("Evangeline");
+	}
+
+	public void F() {
+		System.out.println("Frank");
+	}
+}
